13808da571a357a3586d6c56eb20226f439d5732
Madison <mph7210@gmail.com>
2023-02-09 12:46:55 -0500

    add functionality for one point

---
+++ main.js
@@ -0,0 +1,26 @@
+// JS code for webpage interaction
+
+// Toggle the hovered class of a point
+function pointHovered() {
+    let point = document.getElementById("point");
+    point.classList.toggle("hovered");
+}
+
+// Toggle the clicked class of a point and the visibility of its corresponding text
+function pointClicked() {
+    console.log("clicked")
+    let point = document.getElementById("point");
+    point.classList.toggle("clicked"); 
+    let text = document.getElementById("pointText");
+    text.classList.toggle("invisible");
+}
+
+// Adding hover functionality
+document.getElementById("point")
+            .addEventListener("mouseover", pointHovered);
+document.getElementById("point")
+            .addEventListener("mouseout", pointHovered);
+
+// Adding click functionality
+document.getElementById("point")
+            .addEventListener("click", pointClicked);
